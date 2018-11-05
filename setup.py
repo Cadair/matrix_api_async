480ab578eb3f684ac4c3d150699b2d9673d6303d
Stuart Mumford <stuart@cadair.com>
2018-11-05 12:55:18 +0000

    cleanup python versions

--- setup.py
+++ setup.py
@@ -28,15 +28,12 @@ setup(
     packages=find_packages(exclude=('tests',)),
 
     install_requires=["matrix_client"],
+    python_requires=">=3.5",
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
